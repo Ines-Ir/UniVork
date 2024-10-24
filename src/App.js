758683e262c6180cf71a63b6ec44fa43f4a5f3e8
Ines Iraoui <ines.iraoui.02@gmail.com>
2024-10-23 21:27:46 -0400

    Login Page details finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import AuthPage from './components/AuthPage'; // Import the AuthPage component
+import SignUpPage from './components/SignUpPage'; // Import the SignUpPage component
+import { Navbar, Nav } from 'react-bootstrap'; // Import Bootstrap components
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand as={Link} to="/">UniVork</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto"> {/* Adjusted to use Bootstrap 5 margin utility */}
+            <Nav.Link as={Link} to="/">Login</Nav.Link>
+            <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      
+      <Routes>
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/" element={<AuthPage />} /> {/* Ensure AuthPage is the correct component */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
